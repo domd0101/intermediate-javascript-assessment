ddfe4452d27dd2930ac8e089229b1490e2b71959
domd0101 <ddecicco@buffalo.edu>
2017-03-31 16:15:41 -0600

    finished

--- src/assessment-part1.js
+++ src/assessment-part1.js
@@ -43,27 +43,27 @@ function daBears(){
 // Which function(s) access the "chair" variable and get "Too Big!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale1 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale1 = ["mamaBear", "papaBear"];
 
 // Which function(s) access the "feeling" variable and get "Hungry"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale2 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale2 = ["goldilocks"];
 
 // Which function(s) access the "porridge" variable and get "Too Cold!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale3 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale3 = ["mamaBear"];
 
 // Which function(s) access the "sleepy" variable and get undefined
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale4 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale4 = ["daBears", "papaBear", "mamaBear", "goldilocks"];
 
 // Which function(s) access the isFurry variable and get true
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear"];
 
 
 // *************
@@ -83,7 +83,19 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 // CODE HERE...
 
+function Vehicle(){
+  this.gasRemaining = 100;
+}
+Vehicle.prototype.drive = function(){
+  this.gasRemaining -= 25;
+}
 
+var charger = new Vehicle();
+var mustang = new Vehicle();
+
+charger.drive();
+mustang.drive();
+mustang.drive();
 
 
 
@@ -104,10 +116,43 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // Your method may be passed punctuation, numbers or other non-letter characters
 // and should neither modify them nor break when encountering them.
 
-
-
-
 // CODE HERE...
+
+    // grammarPolice = function(str){
+String.prototype.grammarPolice = function(str){
+  //  var fil = str.split('');
+  //  var old = [];
+  //  var num = [];
+  //  for (var i = 0; i < fil.length; i++) {
+  //    if(fil[i].search(/[a-z]/i)===0){
+  //      old.push(fil[i]);
+  //    }
+  //  }
+  //
+  // var newstr = str.replace(/[^a-zA-Z ]/g, "").replace(/\d+/g, '');
+  // var slash = '/\/';
+  // var n = str.search(slash);
+  //
+  // str[n] = '\\';
+  // str = str.replace(/&/g, '&amp;') /* This MUST be the 1st replacement. */
+  //            .replace(/'/g, '&apos;') /* The 4 other predefined entities, required. */
+  //            .replace(/"/g, '&quot;')
+  //            .replace(/\\/g, '\\\\')
+  //            .replace(/</g, '&lt;')
+  //            .replace(/>/g, '&gt;').replace(/\u0000/g, '\\0');
+
+  var newstr = str.split(' ');
+  console.log(newstr);
+  for (var i = 0; i < newstr.length; i++) {
+    if(newstr[i]=='\\' ){
+      i++;
+    }
+    newstr[i] = newstr[i].charAt(0).toUpperCase() + newstr[i].toLowerCase().slice(1);
+  }
+  return newstr.join(' ')
+}
+
+// console.log(grammarPolice("`\\ pp ii eldk"));
 
 
 
@@ -127,6 +172,18 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 // CODE HERE...
 
+function valueType(a,b){
+  if(a === b){
+    return "Exactly the same"
+  }
+  else if(a == b){
+    return "Same value, different types"
+  }
+  else {
+    return "Different values"
+  }
+}
+
 
 
 // *************
@@ -141,3 +198,11 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 var theAnswer = "Unknown";
 
 // CODE HERE...
+
+function promiseCatcher(para){
+var promise = para;
+promise.then(function(result){
+  theAnswer = result;
+})
+
+}
